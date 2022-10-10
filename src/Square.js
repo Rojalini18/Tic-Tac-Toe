42ee296704035bf248001b51efa9d8f664c2140e
Rojalini Das <rojalini08das@gmail.com>
2022-10-10 23:18:00 +0530

    tic-tac-toe

--- src/Square.js
+++ src/Square.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Square(props) {
-    const classes =props.className?`${props.className} square`:'square';
+  const classes = props.className ? `${props.className} square` : "square";
   return (
-   <span className={classes} onClick={props.onClick}>
-       {props.state}
-   </span>
+    <span className={classes} onClick={props.onClick}>
+      {props.state}
+    </span>
   );
 }
 
